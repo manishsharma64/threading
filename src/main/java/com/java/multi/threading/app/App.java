1113a2276254d8f422888955e1a13f2fdcb99a72
Manish Sharma <manishsharma64@gmail.com>
2016-08-08 17:09:01 +0800

    Initial Commit

---
+++ src/main/java/com/java/multi/threading/app/App.java
@@ -0,0 +1,13 @@
+package com.java.multi.threading.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
